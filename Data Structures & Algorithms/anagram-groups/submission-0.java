bcac440d9ac22dfd1830ffbec0013bcc5a974fdf
ACHRAF SAADALI <achrafsaadalii@gmail.com>
2026-06-21 11:46:12 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       
+        HashMap<String , List<String>> map = new HashMap<>();
+
+        for(String s : strs)
+        {  
+            char[] chararray  = s.toCharArray();
+            Arrays.sort(chararray);
+            String backString = new String(chararray);
+            List<String> L = map.getOrDefault(backString,new ArrayList<>());
+            L.add(s);
+            map.put(backString,L);
+
+        }
+        List<List<String>> theReturnArray = new ArrayList<>();
+        for(List<String> L : map.values()) theReturnArray.add(L);
+        return theReturnArray ; 
+        
+    }
+}
