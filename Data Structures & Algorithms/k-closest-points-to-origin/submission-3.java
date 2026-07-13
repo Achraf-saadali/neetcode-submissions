61ea070a0b5ce7b36beca240c8add74a28fd71f3
ACHRAF SAADALI <achrafsaadalii@gmail.com>
2026-07-13 13:02:13 +0100

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Comparator<int[]> comp = (p1, p2) -> {
+    int d1 = p1[0] * p1[0] + p1[1] * p1[1];
+    int d2 = p2[0] * p2[0] + p2[1] * p2[1];
+    return Integer.compare(d1, d2);
+};
+        PriorityQueue<int[]> q = new PriorityQueue<>(k,comp);
+        for(int  i = points.length-1 ; i >= 0 ;i-- )
+        {
+            q.offer(points[i]);
+            
+        }
+
+
+        int[] result[] = new int[k][2];
+
+        while(k > 0 ) result[--k] = q.poll();
+
+        return result ; 
+
+    
+    
+
+
+
+
+
+     
+
+
+
+
+
+     
+
+
+
+
+        
+
+    }
+  
+}
